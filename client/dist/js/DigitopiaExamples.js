030ed94df330d8602329b778c9da69c8ad8a4d10
Michael Rhodes <mrhodes@mediapolis.com>
2016-02-29 20:51:43 -0400

    dropzone upload

--- client/dist/js/DigitopiaExamples.js
+++ client/dist/js/DigitopiaExamples.js
@@ -16886,13 +16886,16 @@ function GetJQueryPlugin(classname,obj) {
 
 })(window, document);
 ;(function ($) {
+	// disable dropzone auto instantiation
 	Dropzone.autoDiscover = false;
 
+	// define dropzoneController
 	function dropzoneController(elem, options) {
 		this.element = $(elem);
 
 		var self = this;
 
+		// get the upload endpoint from the html data-endpoint= tag
 		this.settings = $.extend({
 			endpoint: this.element.data('endpoint')
 		}, options || {});
@@ -16900,6 +16903,7 @@ function GetJQueryPlugin(classname,obj) {
 		this.dropzone = undefined;
 
 		this.start = function () {
+
 			this.dropzone = new Dropzone(this.element[0], {
 				url: self.settings.endpoint,
 				paramName: 'uploadedFile',
@@ -16908,15 +16912,18 @@ function GetJQueryPlugin(classname,obj) {
 
 				init: function () {
 
+					// only allow single file upload
 					this.on('maxfilesexceeded', function (file) {
 						this.removeAllFiles();
 						this.addFile(file);
 					});
 
+					// provide feedback that upload is processing
 					this.on("processing", function () {
 						self.element.addClass('loading');
 					});
 
+					// on success, update the current image in the markup
 					this.on("success", function (dzfile, body) {
 						var response = body.response;
 						var s3file = response.url;
@@ -16926,11 +16933,13 @@ function GetJQueryPlugin(classname,obj) {
 						self.element.removeClass('loading');
 					});
 
+					// remove the thumbnail from the dropzone UI
 					this.on("complete", function (file) {
 						this.removeFile(file);
 					});
 				}
 			});
+
 		};
 
 		this.stop = function () {};
--- server/lib/getUploadForProperty.js
+++ server/lib/getUploadForProperty.js
@@ -1,3 +1,8 @@
+// this function expects
+//  prop: property that was specified when the image was uploaded eg. "photo" "background"
+//  uploads: array of Uploads to scan
+// if nothing is found it returns a slug graphic (/images/fpo.jpg)
+// otherwise it returns the url of the desired upload
 module.exports = function getUploadForProperty(prop, uploads) {
 	if (uploads && uploads.length) {
 		for (var j = 0; j < uploads.length; j++) {
